5ed4eb4bbb7b940f67b0d33bb384f1b81580df05
tiamed <tiamedue@gmail.com>
2019-03-25 10:37:30 +0800

    add client

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
 
 const app = express();
 
+// Allow cross-origon
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true
+  schema: schema,
+  graphiql: true
 }));
 
 const PORT = process.env.PORT || 5000;
