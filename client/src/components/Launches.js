8b0acf7704cdda080217b287654e5f4029600042
tiamed <tiamedue@gmail.com>
2019-03-25 11:05:00 +0800

    switch to production

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,18 +1,18 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react'
 import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
+import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
-import MissionKey from './MissionKey';
+import MissionKey from './MissionKey'
 
 const LAUNCHES_QUERY = gql`
-  query LaunchesQuery{
-    launches {
-      flight_number
-      mission_name
-      launch_date_local
-      launch_success
+    query LaunchesQuery{
+        launches {
+            flight_number
+            mission_name
+            launch_date_local
+            launch_success
+        }
     }
-  }
 `;
 
 export class Launches extends Component {
@@ -22,23 +22,23 @@ export class Launches extends Component {
         <h1 className="display-4 my-3">Launches</h1>
         <MissionKey></MissionKey>
         <Query query={LAUNCHES_QUERY}>
-          {
-            ({loading, error, data}) => {
-              if (loading) return <h4>Loading...</h4>;
-              if (error) console.log(error);
-              return <Fragment>
-                {
-                  data.launches.map(launch => (
-                    <LaunchItem key={launch.flight_number} launch={launch}/>
-                  ))
-                }
-              </Fragment>;
+            {
+              ({ loading, error, data }) => {
+                if(loading) return <h4>Loading...</h4>;
+                if(error) console.log(error);
+                return <Fragment>
+                  {
+                    data.launches.map(launch => (
+                      <LaunchItem key={launch.flight_number} launch={launch}/>
+                    ))
+                  }
+                </Fragment>;
+              }
             }
-          }
         </Query>
       </Fragment>
-    );
+    )
   }
 }
 
-export default Launches;
+export default Launches
